8613370822d4e0346ef7959efa7fdd2680aa8b99
jonahshader <jonah.shader@gmail.com>
2019-11-08 16:16:54 -0500

    tweaked expressions, added code for super sampling

--- src/App.kt
+++ src/App.kt
@@ -9,33 +9,49 @@ fun getDelta(x: Double, y: Double, expr1: (Double, Double) -> (Double), expr2: (
 fun getColor(x: Double, y: Double, expr1: (Double, Double) -> (Double), expr2: (Double, Double) -> (Double)) : Double = max(255.0 - (getDelta(x, y, expr1, expr2)).absoluteValue * 256, 0.0)
 
 class App : PApplet() {
-    val expr1: (Double, Double) -> (Double) = { x, y -> cos(x)/y }
-    val expr2: (Double, Double) -> (Double) = { x, y -> cos(y)/x }
+    val expr1: (Double, Double) -> (Double) = { x, y -> log(x, y) }
+    val expr2: (Double, Double) -> (Double) = { x, y -> log(sin(y), (x*y).absoluteValue) }
 
     var currentX = 0
     var currentY = 0
 
     override fun settings() {
-        size(512, 512)
+        size(1024, 1024)
         noSmooth()
     }
 
     override fun setup() {
         background(0)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                var pixelBrightness = 0f
+//                for (xKernel in -1..1) {
+//                    for (yKernel in -1..1) {
+//                        val newX = x + xKernel / 3.0
+//                        val newY = y + yKernel / 3.0
+//                        pixelBrightness += getColor((newX - (width / 2)) * .125 * .125, -(newY - (height / 2)) * .125 * .125, expr1, expr2).toFloat()
+//                    }
+//                }
+//                pixelBrightness /= 9.0f
+                pixelBrightness = getColor((x - (width / 2)).toDouble() * .125 * .125, -(y - (height / 2)).toDouble() * .125 * .125, expr1, expr2).toFloat()
+                stroke(pixelBrightness, pixelBrightness, pixelBrightness)
+                point(x.toFloat(), y.toFloat())
+            }
+        }
     }
 
     override fun draw() {
-        for (i in 0..1024) {
-            if (currentY < height) {
-                val pixelBrightness: Float = getColor((currentX - (width / 2)).toDouble() * .125 * .25, (currentY - (height / 2)).toDouble() * .125 * .25, expr1, expr2).toFloat()
-                stroke(pixelBrightness, pixelBrightness, pixelBrightness)
-                point(currentX.toFloat(), currentY.toFloat())
-                currentX++
-                if (currentX > width) {
-                    currentX = 0
-                    currentY++
-                }
-            }
-        }
+//        for (i in 0.) {
+//            if (currentY < height) {
+//                val pixelBrightness: Float = getColor((currentX - (width / 2)).toDouble() * .125 * .25, (currentY - (height / 2)).toDouble() * .125 * .25, expr1, expr2).toFloat()
+//                stroke(pixelBrightness, pixelBrightness, pixelBrightness)
+//                point(currentX.toFloat(), currentY.toFloat())
+//                currentX++
+//                if (currentX > width) {
+//                    currentX = 0
+//                    currentY++
+//                }
+//            }
+//        }
     }
 }
