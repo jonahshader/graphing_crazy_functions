8b3364d2b53b5da64a31167a480f726b20d4ab6b
jonahshader <jonah.shader@gmail.com>
2019-12-03 13:15:31 -0500

    sped-up with parallel coroutines

--- src/App.kt
+++ src/App.kt
@@ -1,4 +1,6 @@
+import kotlinx.coroutines.GlobalScope
 import processing.core.PApplet
+import kotlin.concurrent.thread
 import kotlin.math.*
 
 fun main() {
@@ -9,22 +11,24 @@ fun getDelta(x: Double, y: Double, expr1: (Double, Double) -> (Double), expr2: (
 fun getColor(x: Double, y: Double, expr1: (Double, Double) -> (Double), expr2: (Double, Double) -> (Double)) : Double = max(255.0 - (getDelta(x, y, expr1, expr2)).absoluteValue * 512, 0.0)
 
 class App : PApplet() {
-    val expr1: (Double, Double) -> (Double) = { x, y -> cos(x.pow(2)) }
+    val expr1: (Double, Double) -> (Double) = { x, y -> log(cos(x.pow(2)), y.pow(2)) }
     val expr2: (Double, Double) -> (Double) = { x, y -> cos(y.pow(2)) }
 
     var currentX = 0
     var currentY = 0
 
     override fun settings() {
-        size(2560 * 2, 1440 * 2)
+        size(640, 640)
         noSmooth()
     }
 
     override fun setup() {
         background(0)
+        loadPixels()
         for (x in 0 until width) {
-            for (y in 0 until height) {
-                var pixelBrightness = 0f
+            GlobalScope.run {
+                for (y in 0 until height) {
+                    var pixelBrightness = 0f
 //                for (xKernel in -1..1) {
 //                    for (yKernel in -1..1) {
 //                        val newX = x + xKernel / 3.0
@@ -33,11 +37,15 @@ class App : PApplet() {
 //                    }
 //                }
 //                pixelBrightness /= 9.0f
-                pixelBrightness = getColor((x - (width / 2)).toDouble() * .125.pow(2), -(y - (height / 2)).toDouble() * .125.pow(2), expr1, expr2).toFloat()
-                stroke(pixelBrightness, pixelBrightness, pixelBrightness)
-                point(x.toFloat(), y.toFloat())
+                    pixelBrightness = getColor((x - (width / 2)).toDouble() * .125.pow(2), -(y - (height / 2)).toDouble() * .125.pow(2), expr1, expr2).toFloat()
+//                stroke(pixelBrightness, pixelBrightness, pixelBrightness)
+//                point(x.toFloat(), y.toFloat())
+                    pixels[x * width + y] = color(pixelBrightness)
+                }
             }
+
         }
+        updatePixels()
     }
 
     override fun draw() {
